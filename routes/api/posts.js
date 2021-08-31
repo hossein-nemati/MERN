bff322d860d43f660d43a57ed7aa96d3b9e022c0
hossein-nemati <80382370+hossein-nemati@users.noreply.github.com>
2021-08-31 21:38:46 +0430

    current user profile route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-// @route  GET api/posts/test
-// @desc   Test post route
+// @route GET api/posts/test
+// @desc  Test post route
 // @access Public
 router.get('/test', (req, res) => res.json({msg: "Users Posts"}));
 
