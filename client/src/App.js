71b96a1559f3334e9d97c3e2d0cb6b84b798bd4c
hossein-nemati <80382370+hossein-nemati@users.noreply.github.com>
2021-09-14 17:01:50 +0430

    add layout files

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
+import React, { Component } from 'react';
+
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Landing from './components/layout/Landing';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>My React App</h1>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <Landing />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
