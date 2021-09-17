fa94753a6eef6d3af6a64f7e90159375b6a6167d
hossein-nemati <80382370+hossein-nemati@users.noreply.github.com>
2021-09-17 15:17:08 +0430

    Redux Login Action & Set Current User

--- client/src/reducer/authReducer.js
+++ client/src/reducer/authReducer.js
@@ -1,11 +1,23 @@
-import { GET_ERRORS } from "../actions/types";
+import isEmpty from "../validation/is-empty";
 
-const initialState = {};
+import { GET_ERRORS } from "../actions/types";
+import { SET_CURRENT_USER } from "../actions/types";
+
+const initialState = {
+    isAuthenticate: false,
+    user: {}
+};
 
 export default function authReducer(state = initialState, action) {
     switch(action.type) {
         case GET_ERRORS:
             return action.payload;
+        case SET_CURRENT_USER:
+            return {
+                ...state,
+                isAuthenticate: !isEmpty(action.payload),
+                user: action.payload
+            };
         default:
             return state;
     }
