6773a1569a863088632d2f2ab2059bd966a9398b
hossein-nemati <80382370+hossein-nemati@users.noreply.github.com>
2021-09-30 12:19:07 +0330

    create profile action

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import TextFieldGroup from '../common/TextFieldGroup';
 import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
 import InputGroup from '../common/InputGroup';
 import SelectListGroup from '../common/SelectListGroup';
+import { createProfile } from '../../actions/profileActions';
 
 class CreateProfile extends Component {
     constructor(props) {
@@ -31,9 +33,29 @@ class CreateProfile extends Component {
         this.onSubmit = this.onSubmit.bind(this);
     }
 
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.errors) {
+            this.setState({errors: nextProps.errors});
+        }
+    }
+
     onSubmit(e) {
         e.preventDefault();
-        console.log('submit');
+
+        const profileData = {
+            handle: this.state.handle,
+            company: this.state.company,
+            website: this.state.website,
+            location: this.state.location,
+            status: this.state.status,
+            skills: this.state.skills,
+            bio: this.state.bio,
+            twitter: this.state.twitter,
+            linkedin: this.state.linkedin,
+            youtube: this.state.youtube
+        }
+
+        this.props.createProfile(profileData, this.props.history);
     }
 
     onChange(e) {
@@ -142,6 +164,7 @@ class CreateProfile extends Component {
                                 />
                                 <div className="mb-3">
                                     <button
+                                        type="button"
                                         onClick={() => {
                                             this.setState(prevState => ({
                                                 displaySocialInputs: !prevState.displaySocialInputs
@@ -174,4 +197,4 @@ const mapStateToProps = state => ({
     errors: state.errors
 });
 
-export default connect(mapStateToProps)(CreateProfile);
+export default connect(mapStateToProps, { createProfile })(withRouter(CreateProfile));
